567eadf5c96a76dc8635c102232aa751246a719b
Ankit Solanki <ankit_ms95@yahoo.com>
2020-11-25 17:45:17 +0530

    Initial Commit

---
+++ ios-swiftui-views/ios_swiftui_viewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_swiftui_viewsApp.swift
+//  ios-swiftui-views
+//
+//  Created by Ankit Solanki on 25/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ios_swiftui_viewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
